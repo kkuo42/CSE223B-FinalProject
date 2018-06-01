c5886c89ba1cabc04c14ad42132ccb25b9cd2da7
Kevin Kuo <kevinstatusk@gmail.com>
2018-05-31 21:53:52 -0700

    fixed keeper.go formatting

--- keeper.go
+++ keeper.go
@@ -85,7 +85,7 @@ func (k *KeeperClient) GetBackends() ([]*ClientFs, []*ClientFs, error) {
 	return k.servercoords, k.serverfs, nil
 }
 
-func (k *KeeperClient) UpdateBackends() (error) {
+func (k *KeeperClient) UpdateBackends() error {
 	coordbacks, _, _, e := k.client.ChildrenW("/alivecoord")
 	if e != nil {
 		log.Fatalf("error getting alive nodes", e)
@@ -116,132 +116,134 @@ func (k *KeeperClient) UpdateBackends() (error) {
 			}
 			serverfs = append(serverfs, c)
 			done <- true
-			}(addr)
-			go func(a string) {
-				c := NewClientFs(a)
-				// if it is not this server then connect
-				if a != k.fsaddr {
-					e := c.Connect()
-					if e != nil {
-						log.Println("keeper couldnt connect to backend", a)
-					}
-				}
-				servercoords = append(servercoords, c)
-				done <- true
-				}(coordbacks[i])
-			}
+		}(addr)
 
-			for i := 0; i < len(fsbacks) + len(coordbacks); i++ {
-				<-done
-			}
-
-			if len(servercoords) != len(serverfs) {
-				return fmt.Errorf("Coords didnt match ServerFS\n")
-			}
-			if len(servercoords) == 0 {
-				return fmt.Errorf("No backends online yet\n")
-			}
-			k.servercoords = servercoords
-			k.serverfs = serverfs
-			return nil
-		}
-
-		func (k *KeeperClient) Get(path string) (KeeperMeta, error) {
-			data, _, e := k.client.Get("/data/" + path)
-			if e != nil {
-				// do nothing for now, should crash
-				return KeeperMeta{}, e
-			}
-			var kmeta KeeperMeta
-			e = json.Unmarshal(data, &kmeta)
-			return kmeta, nil
-		}
-
-		func (k *KeeperClient) Set(path string, data KeeperMeta) error {
-			m, e := json.Marshal(&data)
-			_, e = k.client.Set("/data/"+path, m, -1)
-			if e != nil {
-				return e
-			}
-			return nil
-		}
-
-		func (k *KeeperClient) GetChildren(path string) ([]string, error) {
-			// if in the root directory don't add /
-			inputstr := "/data"
-			if path != "" {
-				inputstr += "/"+path
-			}
-
-			files, _, e := k.client.Children(inputstr)
-			if e != nil {
-				return nil, e
-			}
-
-			return files, nil
-		}
-
-		func (k *KeeperClient) GetChildrenAttributes(path string) ([]fuse.DirEntry, error) {
-			files, e := k.GetChildren(path)
-
-			if e != nil {
-				return nil, e
-			}
-			fileEntries := []fuse.DirEntry{}
-
-			// for each of the files fetched get their metadata (may be unecessary)
-			for _, f := range files {
-				p := f
-				if path != "" {
-					p = path + "/" + f
-				}
-				fm, e := k.Get(p)
+		go func(a string) {
+			c := NewClientFs(a)
+			// if it is not this server then connect
+			if a != k.fsaddr {
+				e := c.Connect()
 				if e != nil {
-					log.Println("error here:", e)
-					return nil, e
+					log.Println("keeper couldnt connect to backend", a)
 				}
-				fileEntries = append(fileEntries, fuse.DirEntry{Name: f, Mode: fm.Attr.Mode})
 			}
-			return fileEntries, nil
-		}
+			servercoords = append(servercoords, c)
+			done <- true
+		}(coordbacks[i])
+	}
 
-		func (k *KeeperClient) Create(path string, attr fuse.Attr) error {
-			kmeta := KeeperMeta{Primary: k.coordaddr, Attr: attr}
-			d, e := json.Marshal(&kmeta)
+	for i := 0; i < len(fsbacks) + len(coordbacks); i++ {
+		<-done
+	}
+
+	if len(servercoords) != len(serverfs) {
+		return fmt.Errorf("Coords didnt match ServerFS\n")
+	}
+	if len(servercoords) == 0 {
+		return fmt.Errorf("No backends online yet\n")
+	}
+
+	k.servercoords = servercoords
+	k.serverfs = serverfs
+	return nil
+}
+
+func (k *KeeperClient) Get(path string) (KeeperMeta, error) {
+	data, _, e := k.client.Get("/data/" + path)
+	if e != nil {
+		// do nothing for now, should crash
+		return KeeperMeta{}, e
+	}
+	var kmeta KeeperMeta
+	e = json.Unmarshal(data, &kmeta)
+	return kmeta, nil
+}
+
+func (k *KeeperClient) Set(path string, data KeeperMeta) error {
+	m, e := json.Marshal(&data)
+	_, e = k.client.Set("/data/"+path, m, -1)
+	if e != nil {
+		return e
+	}
+	return nil
+}
+
+func (k *KeeperClient) GetChildren(path string) ([]string, error) {
+	// if in the root directory don't add /
+	inputstr := "/data"
+	if path != "" {
+		inputstr += "/"+path
+	}
+
+	files, _, e := k.client.Children(inputstr)
+	if e != nil {
+		return nil, e
+	}
+
+	return files, nil
+}
+
+func (k *KeeperClient) GetChildrenAttributes(path string) ([]fuse.DirEntry, error) {
+	files, e := k.GetChildren(path)
+
+	if e != nil {
+		return nil, e
+	}
+	fileEntries := []fuse.DirEntry{}
+
+	// for each of the files fetched get their metadata (may be unecessary)
+	for _, f := range files {
+		p := f
+		if path != "" {
+			p = path + "/" + f
+		}
+		fm, e := k.Get(p)
+		if e != nil {
+			log.Println("error here:", e)
+			return nil, e
+		}
+		fileEntries = append(fileEntries, fuse.DirEntry{Name: f, Mode: fm.Attr.Mode})
+	}
+	return fileEntries, nil
+}
+
+func (k *KeeperClient) Create(path string, attr fuse.Attr) error {
+	kmeta := KeeperMeta{Primary: k.coordaddr, Attr: attr}
+	d, e := json.Marshal(&kmeta)
+	if e != nil {
+		return e
+	}
+
+	_, e = k.client.Create("/data/"+path, []byte(d), int32(0), zk.WorldACL(zk.PermAll))
+	if e != nil {
+		return e
+	}
+	return nil
+}
+
+func (k *KeeperClient) Remove(path string) error {
+	err := k.client.Delete("/data/"+path, -1)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (k *KeeperClient) RemoveDir(path string) error {
+	// recursively delete all children
+	children, e := k.GetChildren(path)
+	if len(children) == 0 || e != nil {
+		// it is safe to delete this node
+		k.Remove(path)
+	} else {
+		// go through each child and recursively delete on it
+		for _, f := range children {
+			e := k.RemoveDir(path+"/"+f)
 			if e != nil {
 				return e
 			}
-
-			_, e = k.client.Create("/data/"+path, []byte(d), int32(0), zk.WorldACL(zk.PermAll))
-			if e != nil {
-				return e
-			}
-			return nil
 		}
-
-		func (k *KeeperClient) Remove(path string) error {
-			err := k.client.Delete("/data/"+path, -1)
-
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-
-		func (k *KeeperClient) RemoveDir(path string) error {
-			// recursively delete all children
-			children, e := k.GetChildren(path)
-			if len(children) == 0 || e != nil {
-				// it is safe to delete this node
-				k.Remove(path)
-			} else {
-				// go through each child and recursively delete on it
-				for _, f := range children {
-					e := k.RemoveDir(path+"/"+f)
-					if e != nil {
-						return e
-					}
-				}
-			}
-			return nil
-		}
+	}
+	return nil
+}
