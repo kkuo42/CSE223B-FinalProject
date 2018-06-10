1cac85d215a2f0ffe2bf3664ffefb8074747dc25
CJ Pais <cjpaisdev@gmail.com>
2018-06-09 19:10:34 -0700

    config

--- config.go
+++ config.go
@@ -6,14 +6,15 @@ import (
 
 //var ZkAddrs = []string{"54.197.196.191:2181"}
 //var ZkAddrs = []string{"localhost:2181"}
-var ZkAddrs = []string{"vm166.sysnet.ucsd.edu:2181"}
+//var ZkAddrs = []string{"vm166.sysnet.ucsd.edu:2181"}
+var ZkAddrs = []string{"ubuntu@ec2-34-219-144-16.us-west-2.compute.amazonaws.com"}
 var Debug = true
 //var Debug = false
 // go from your ip addr to location in sync array
 var ReplicaAddrs = map[string]string{"localhost:9600": "localhost:9601", "localhost:9601": "localhost:9602", "localhost:9602": "localhost:9600"}
 var GeoFSMode = uint32(0755)
 var SequentialEphemeral = int32(3)
-var BalanceTime = 30 * time.Second
+var BalanceTime = 60 * time.Second
 
 // potential artificial latency in milliseconds
 var Latency = 100
