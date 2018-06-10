9a40f6bf03b4066fef0d90ecedcd242897ec6f75
CJ Pais <cjpaisdev@gmail.com>
2018-06-10 14:26:27 -0700

    awswrite

---
+++ perf/awswrite.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+WRITE_NUM = 10
+
+start_time = time.time()
+for i in range(WRITE_NUM):
+	print ("writing ", i, "th time")
+	os.system("echo -n a >> data/toserver1/a")
+elapsed_time = time.time() - start_time
+print ("serial write:")
+print ("\ttotal time:\t", elapsed_time, "seconds")
+print ("\twrites/second:\t", elapsed_time)
+
